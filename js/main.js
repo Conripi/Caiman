97fd0899854f72d7b8729951b55ef6373bd398e6
Conripi <mlksanno@gmail.com>
2021-02-27 15:17:28 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-alert("!");
+
+
+$(".twitter").on("click", function () {
+    location.href = 'https://twitter.com/Conripi1230';
+})
+
+$(".kyoushin").on("click", function () {
+    location.href = 'https://www.bosai.go.jp/';
+})
+
+$(".youtube").on("click", function () {
+    location.href = 'https://www.youtube.com/channel/UCPL3kFog2fXKp0l8LOg8pMw';
+})
